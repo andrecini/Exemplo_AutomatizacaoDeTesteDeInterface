48a9b1523abfb5f8f4fb0a66c21a31b8099660ab
André Cini <andrevpcini@gmail.com>
2021-07-22 12:24:32 -0300

    Falta arrumar a classe Escritor e Fazer o teste de interface

---
+++ CadastroDeAluno/Classes/Aluno.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CadastroDeAluno
+{
+    public class Aluno
+    {
+        #region Encapsulamento
+
+        private string _nome;
+        private string _nascimento;
+        private string _telefone;
+        private string _cpf;
+        private string _uf;
+        private string _ra;
+        private string _periodo;
+
+        #endregion
+
+        #region Propriedades
+        public string Nome
+        {
+            get => _nome;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("O campo 'Nome' não foi preenchido corretamente!");
+                else
+                    _nome = value;
+            }
+        }
+
+        public string Nascimento
+        {
+            get => _nascimento;
+            set
+            {
+                _nascimento = value;
+            }
+        }
+
+        public string Telefone
+        {
+            get => _telefone;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("O campo 'Telefone' não foi preenchido corretamente!");
+                else
+                    _telefone = value;
+            }
+        }
+
+        public string CPF
+        {
+            get => _cpf;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("O campo 'CPF' não foi preenchido corretamente!");
+                else
+                    _cpf = value;
+            }
+        }
+
+        public string UF
+        {
+            get => _uf;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("O campo 'UF' não foi preenchido corretamente!");
+                else
+                    _uf = value;
+            }
+        }
+
+        public string RA
+        {
+            get => _ra;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("O campo 'RA' não foi preenchido corretamente!");
+                else
+                    _ra = value;
+            }
+        }
+
+        public string Periodo
+        {
+            get => _periodo;
+            set
+            {
+                if (string.IsNullOrEmpty(value))
+                    throw new Exception("O campo 'Periodo' não foi preenchido corretamente!");
+                else
+                    _periodo = value;
+            }
+        }
+        #endregion
+
+        #region Construtores
+
+        public Aluno() { }
+
+        public Aluno(ICollection<string> dados)
+        {
+            Nome = dados.ElementAt(0);
+            Nascimento = dados.ElementAt(1);
+            Telefone = dados.ElementAt(2);
+            CPF = dados.ElementAt(3);
+            UF = dados.ElementAt(4);
+            RA = dados.ElementAt(5);
+            Periodo = dados.ElementAt(6);
+        }
+
+        #endregion
+
+    }
+}
---
+++ CadastroDeAluno/Classes/Escritor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace CadastroDeAluno.Classes
+{
+    public class Escritor
+    {
+        public static void SalvaArquivo(List<Aluno> cadastrados)
+        {
+            if (!Directory.Exists(@"\AlunosCadastrados"))
+                Directory.CreateDirectory(@"\AlunosCadastrados");
+
+            File.WriteAllText("Registros", FormataConteudo(cadastrados));
+        }
+
+        private static string FormataConteudo(List<Aluno> cadastrados)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var a in cadastrados)
+            {
+                sb.AppendLine(string.Format("Nome: {0}", a.Nome));
+                sb.AppendLine(string.Format("Data de Nascimento: {0}", a.Nascimento));
+                sb.AppendLine(string.Format("Telefone: {0}", a.Telefone));
+                sb.AppendLine(string.Format("CPF: {0}", a.CPF));
+                sb.AppendLine(string.Format("UF: {0}", a.UF));
+                sb.AppendLine(string.Format("RA: {0}", a.RA));
+                sb.AppendLine(string.Format("Período: {0}", a.Periodo));
+                sb.AppendLine("\n");
+            }
+
+            return sb.ToString();
+
+        }
+    }
+}
